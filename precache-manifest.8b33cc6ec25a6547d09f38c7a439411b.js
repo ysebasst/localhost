2e277b1075da58d3d02c933e7ac52c044249d883
Sebastian-Sierra <66404075+Sebastian-Sierra@users.noreply.github.com>
2020-12-17 03:23:52 -0500

    deploy

---
+++ precache-manifest.8b33cc6ec25a6547d09f38c7a439411b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472d87e6931309f31df9",
+    "url": "/localhost/css/app.cc988808.css"
+  },
+  {
+    "revision": "5bdc3580afa6d0cc91772b9ac490b3a3",
+    "url": "/localhost/index.html"
+  },
+  {
+    "revision": "472d87e6931309f31df9",
+    "url": "/localhost/js/app.ba4e0f16.js"
+  },
+  {
+    "revision": "8aba9e47ba17ac99bdb7",
+    "url": "/localhost/js/chunk-vendors.53f66a70.js"
+  },
+  {
+    "revision": "c557a42801c9853c04b392e017a9c385",
+    "url": "/localhost/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/localhost/robots.txt"
+  }
+]);
