ec00dd067737849a56abb423b05c8b5cf663ff31
ysebasst <ysebasst@gmail.com>
2023-03-31 16:56:11 +0000

    deploy

---
+++ precache-manifest.4f97e1532f3e1cf94ef54e3fcf03d38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6419a11c7f1e37cbf45c",
+    "url": "/localhost/css/app.e7bfd973.css"
+  },
+  {
+    "revision": "1343e99e80bdbcf2b826542c515e8b87",
+    "url": "/localhost/index.html"
+  },
+  {
+    "revision": "6419a11c7f1e37cbf45c",
+    "url": "/localhost/js/app.eb613fe8.js"
+  },
+  {
+    "revision": "8aba9e47ba17ac99bdb7",
+    "url": "/localhost/js/chunk-vendors.53f66a70.js"
+  },
+  {
+    "revision": "c557a42801c9853c04b392e017a9c385",
+    "url": "/localhost/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/localhost/robots.txt"
+  }
+]);
